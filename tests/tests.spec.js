55af13ded5f2ab7957f8b5b4d1b1a0ff21a5a055
David Desmaisons <desmaisons_david@hotmail.com>
2021-08-17 12:17:02 -0300

    First commmit

---
+++ tests/tests.spec.js
@@ -0,0 +1,21 @@
+import test from "../node_modules/tape/index.js";
+
+test('first', function (t) {
+
+  setTimeout(function () {
+    t.ok(1, 'first test');
+    t.end();
+  }, 200);
+
+  t.test('second', function (t) {
+    t.ok(1, 'second test');
+    t.end();
+  });
+});
+
+test('third', function (t) {
+  setTimeout(function () {
+    t.ok(1, 'third test');
+    t.end();
+  }, 100);
+});
